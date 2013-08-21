b1b41401339ff82c2043b11c83d032f93169c5dc
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-20 22:56:17 -0400

    updating byGithub index -- missing tests

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 'use strict';
 
 var runnel = require('runnel') 
+  , log = require('valuepack-core/util/log')
   , updateNpm = require('valuepack-mine-npm')
-  , updateGithub = require('valuepack-mine-github');
+  , updateGithub = require('valuepack-mine-github')
+  , getGithubLogins = require('./lib/get-github-logins')
+  , updateByGithub = require('./lib/update-by-github')
+  ;
   
+function getLoginsAndApplyFixes (db, cb) {
+  getGithubLogins(db, [ 'raynos', 'thlorenz', 'tjholowaychuk' ], function (err, logins) {
+    if (err) return cb(err);
+    
+    updateByGithub(db, logins.byOwner, cb);
+  });
+}
 
 // 1. update npm
 
@@ -18,11 +29,30 @@ var runnel = require('runnel')
 
 // at this point we should have mined all data
 
-var go = module.exports = function (db) {
+var go = module.exports = function (db, cb) {
   /*runnel(
       updateNpm
   );*/
 
+ getLoginsAndApplyFixes(db, cb);
 
 };
 
+
+// Test
+if (!module.parent) {
+  log.level = 'silly';
+
+  var leveldb = require('valuepack-core/mine/leveldb')
+  leveldb.open(function (err, db) {
+    if (err) return console.error(err);
+    go(db, function (err) {
+      if (err) return console.error(err);
+      console.log('DONE')  
+    });
+  })
+}
+
+function inspect(obj, depth) {
+  console.log(require('util').inspect(obj, false, depth || 5, true));
+}
